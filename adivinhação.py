1113231245938aa6def6919e2922e708306f30bf
Lucas Corte <corte1994.lc@gmail.com>
2020-06-06 14:44:34 -0300

    - melhoramento da lógica - utilização de elif, além do else e if

--- adivinhação.py
+++ adivinhação.py
@@ -6,9 +6,18 @@ numero_secreto = 42
 
 chute = int(input('Digite o seu número: '))
 
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
 print(f'Você chutou {chute}!')
 
-if (numero_secreto == chute):
+if (acertou):
     print('Você acertou!')
 else:
-    print('Você errou!')
+    if(maior):
+        print('Você Errou! Seu chute foi maior que o número secreto!')
+    elif(menor):
+        print('Você Errou! Seu chute foi menor que o número secreto!')
+
+print('Fim de Jogo!')
