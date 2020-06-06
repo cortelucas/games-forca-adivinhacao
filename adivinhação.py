9604a3112aa09eaf11ef50ca0ecdace56383deb5
Lucas Corte <corte1994.lc@gmail.com>
2020-06-06 15:04:00 -0300

    - melhoramento da lógica - conhecimento do while - incrementação de contador

--- adivinhação.py
+++ adivinhação.py
@@ -3,21 +3,25 @@ print('Seja bem vindo ao jogo de Adivinhação!')
 print('**************************************')
 
 numero_secreto = 42
+total_de_tentativas = 5
+rodada = 1
 
-chute = int(input('Digite o seu número: '))
+while (rodada <= total_de_tentativas):
+    print(f'{rodada}ª tentativa de {total_de_tentativas}!')
+    chute = int(input('Digite o seu número: '))
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-print(f'Você chutou {chute}!')
+    if (acertou):
+        print('Você acertou!')
+    else:
+        if(maior):
+            print('Você Errou! Seu chute foi maior que o número secreto!')
+        elif(menor):
+            print('Você Errou! Seu chute foi menor que o número secreto!')
 
-if (acertou):
-    print('Você acertou!')
-else:
-    if(maior):
-        print('Você Errou! Seu chute foi maior que o número secreto!')
-    elif(menor):
-        print('Você Errou! Seu chute foi menor que o número secreto!')
+    rodada = rodada + 1
 
 print('Fim de Jogo!')
