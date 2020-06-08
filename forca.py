2028be0cee42d791c32fca34cba73cb72902eab8
Lucas Corte <corte1994.lc@gmail.com>
2020-06-08 15:16:36 -0300

    - criando página para escolha do jogo

---
+++ forca.py
@@ -0,0 +1,9 @@
+def jogar():
+    print('*************************************')
+    print('*****Bem vindo ao jogo de Forca!*****')
+    print('*************************************')
+
+    print('Fim do Jogo!')
+
+if(__name__ == '__main__'):
+    jogar()
---
+++ jogos.py
@@ -0,0 +1,23 @@
+import forca
+import adivinhação
+
+def escolhe_jogo():
+    print('*************************************')
+    print('********* Escolha seu Jogo! *********')
+    print('*************************************')
+
+    print('Lista de Jogos: \n'
+          '1 - Forca! \n'
+          '2 - Adivinhação!')
+
+    jogo = int(input('Escolha o jogo: '))
+
+    if (jogo == 1):
+        print('Jogar forca!')
+        forca.jogar()
+    elif (jogo == 2):
+        print('Jogo adiivnhação')
+        adivinhação.jogar()
+
+if(__name__ == '__main__'):
+    escolhe_jogo()
