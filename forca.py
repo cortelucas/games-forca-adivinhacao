27bab41fe67d0cfb83482be00ec5407ad9b91c21
Lucas Corte <corte1994.lc@gmail.com>
2020-06-09 14:16:58 -0300

    - implemento da lógica

--- forca.py
+++ forca.py
@@ -4,19 +4,24 @@ def jogar():
     print('*************************************')
 
     palavra_secreta = 'banana'
+    letras_acertadas = ['_','_','_','_','_','_']
 
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     while (not enforcou and not acertou):
         chute = input('Qual letra? ')
         chute = chute.strip()
 
-        index = 0
+        i = 0
         for letra in palavra_secreta:
             if (chute.upper() == letra.upper()):
-                print(f'Encontrei a letra {letra} na posição {index}!')
-            index += 1
+                letras_acertadas[i] = letra
+            i += 1
+
+        print(letras_acertadas)
 
 
     print('Fim do Jogo!')
