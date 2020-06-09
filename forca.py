552d39bd756e0bf983377f243bbea1877c83f4e8
Lucas Corte <corte1994.lc@gmail.com>
2020-06-09 07:14:28 -0300

    - implemento da lógica

--- forca.py
+++ forca.py
@@ -3,6 +3,22 @@ def jogar():
     print('*****Bem vindo ao jogo de Forca!*****')
     print('*************************************')
 
+    palavra_secreta = 'banana'
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        chute = input('Qual letra? ')
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print(f'Encontrei a letra {letra} na posição {index}!')
+            index += 1
+
+
     print('Fim do Jogo!')
 
 if(__name__ == '__main__'):
